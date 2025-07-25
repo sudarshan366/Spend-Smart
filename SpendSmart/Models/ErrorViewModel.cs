779df30c5156f768a7f90bac57f6e31c0f90aee9
Sudarshan Fullel <sudarshanfullel@Sudarshans-MacBook-Air.local>
2025-07-25 12:54:54 +0545

    project setup

---
+++ SpendSmart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SpendSmart.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ SpendSmart/Models/Expenses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SpendSmart.Models
+{
+	public class Expenses
+	{
+		
+		
+			public int Id { get; set; }
+
+            public decimal  Value { get; set; }
+
+            public required string Description { get; set; }
+
+    
+	}
+}
+
